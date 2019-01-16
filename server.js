cb465fcf3cb8add26d7184d8b3a770f5987dfec6
Binod <binod@Binitas-MacBook-Air.local>
2019-01-16 15:37:45 -0700

    connected my database, can see JSON on browser

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 const Pokemon = require("./model/pokemon");
 
+app.get("/pokemon", (req, res) => {
+    res.send(Pokemon);
+});
+
+
 app.listen(3000, () =>{
     console.log("Listening on port 3000");
 });
